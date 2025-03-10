5db0dcba690b88a0929c8e0042b59c0e06e1cbd5
Juanchivp12 <Juanvillegasse@gmail.com>
2025-03-09 19:29:32 -0500

    Added dictionary for cards
    - Should probably learn how to play blackjack before I keep going
    - Not sure about the rules

--- Game.py
+++ Game.py
@@ -1,5 +1,23 @@
 import random
 
+deck_dict = {
+    'a' : [1, 11],
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '8' : 8,
+    '9' : 9,
+    'J' : 10,
+    'Q' : 10,
+    'K' : 10
+}
+
 class Dealer:
     def __init__(self):
         self.deck = deck
+
+    def hand_cards(self):
+        pass
