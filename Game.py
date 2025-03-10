accb8b2bf37ca3eb7658c8353b37b83d6b5c365f
Juanchivp12 <Juanvillegasse@gmail.com>
2025-03-10 15:38:25 -0500

    Little progress
    - Im learning python by writing this so Im testing out things
    - Also learning blackjack (lol)

--- Game.py
+++ Game.py
@@ -1,23 +1,35 @@
 import random
 
-deck_dict = {
-    'a' : [1, 11],
-    '2' : 2,
-    '3' : 3,
-    '4' : 4,
-    '5' : 5,
-    '6' : 6,
-    '7' : 7,
-    '8' : 8,
-    '9' : 9,
-    'J' : 10,
-    'Q' : 10,
-    'K' : 10
-}
 
 class Dealer:
-    def __init__(self):
+    def __init__(self, deck):
         self.deck = deck
 
     def hand_cards(self):
-        pass
+        random.shuffle(self.deck)
+        return self.deck.pop(0)
+    
+def main():
+    letters_dict = {
+    'J': 10,
+    'Q': 10,
+    'K': 10
+    }
+
+    deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, letters_dict['J'], letters_dict['Q'], letters_dict['K']]
+
+        
+    dealer = Dealer(deck)
+    my_hand = dealer.hand_cards()
+
+    if my_hand == 'A':
+        a_value = int(input("Would you like the value of a to be 1 or 11? "))
+        if a_value == 1:
+            my_hand = 1
+        else:
+            my_hand = 11
+
+if __name__ == "__main__":
+    main()
+
+
